ccbc1e67ef8d126548bd16a85cd337cf71560acb
canhnht <canhnht1709@gmail.com>
2016-09-08 17:25:56 +0800

    Add PM2 and Nginx setup

--- base-setup.sh
+++ base-setup.sh
@@ -5,11 +5,9 @@ echo '==========Base setup: Git=========='
 echo '==========Install Git in Ubuntu=========='
 sudo apt-get update
 sudo apt-get install git-all
-
 echo '==========Check Git=========='
 which git
 git --version
-
 echo '==========Current Git config=========='
 git config --list
 echo '==========Config Git=========='
@@ -18,3 +16,16 @@ git config --global user.email "canhnht1709@gmail.com"
 git config --global core.editor "vi"
 echo '==========New Git config=========='
 git config --list
+
+
+# TODO:
+## Check SSH key and generate new one. https://help.github.com/articles/generating-an-ssh-key/
+echo '==========TODO: Generate SSH key=========='
+
+
+echo '==========Install Nginx=========='
+sudo apt-get update
+sudo apt-get install nginx
+echo '==========Check Nginx=========='
+which nginx
+service nginx status
--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -4,15 +4,22 @@ echo '==========Install nodejs 6.x for Ubuntu=========='
 
 echo '==========Get Nodejs from node-source=========='
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-
 echo '==========Install nodejs=========='
 sudo apt-get install -y nodejs
-
 echo '==========Install build tools to compile addons from npm=========='
 sudo apt-get install -y build-essential
-
 echo '==========Check nodejs and npm=========='
 which nodejs
 node -v
 which npm
 npm -v
+
+
+echo '==========Install PM2=========='
+sudo npm i -g pm2
+echo '==========Check PM2=========='
+which pm2
+pm2 --version
+pm2 list
+echo '==========Run startup script for PM2=========='
+sh -c "$(pm2 startup | sed -n 2p)"
