687a4bb37b55ed88a35b69d5ce8c5b6e43e16665
canhnht <canhnht1709@gmail.com>
2016-09-08 14:57:11 +0800

    Add nodejs setup

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+echo 'Install nodejs 6.x for Ubuntu'
+
+echo 'Get Nodejs from node-source'
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+echo 'Install nodejs'
+sudo apt-get install -y nodejs
+
+echo 'Install build tools to compile addons from npm'
+sudo apt-get install -y build-essential
+
+echo 'Check nodejs and npm'
+which nodejs
+node -v
+which npm
+npm -v
